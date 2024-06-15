98a65fe589ab680e6b557b298b7949a7d08eddde
owcw <165452367+gapehorm@users.noreply.github.com>
2024-06-15 18:56:56 +0530

    Update route.js

--- app/api/sentry-example-api/route.js
+++ app/api/sentry-example-api/route.js
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 
-export const w= "force-dynamic";
-
 // A faulty API route to test Sentry's error monitoring
-export function GET() {
+export function get(req) {
   throw new Error("Sentry Example API Route Error");
+  // The code below won't be reached due to the error thrown above
   return NextResponse.json({ data: "Testing Sentry Error..." });
 }
